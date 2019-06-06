2ef47c3781174487ef45eae17d09f70a9139dafd
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-06-06 10:54:58 -0400

    Second pass solution

--- CountingVotes/counting_votes.py
+++ CountingVotes/counting_votes.py
@@ -8,6 +8,8 @@ def count_votes(votes):
         return votes[0]
     # Create dictionary to store key/value pairs of names and their vote count.
     vote_tally = {}
+    highest_count = 0
+    winner = ""
 
     # Loop over list, check to see if name (as a key) exists in the dictionary. If it doesn't, add it as a new key and add 1 as its starting value. If it does, increase the value by 1.
 
@@ -19,11 +21,17 @@ def count_votes(votes):
         else:
             vote_tally[name.lower()] = 1
 
+        if vote_tally[name] > highest_count:
+            highest_count = vote_tally[name]
+            winner = name
+        elif vote_tally[name] == highest_count:
+            winner = name if name > winner else winner
+
     # Loop through the completed dictionary's values, and return the key with the highest number. If two names have an equal amount of votes, compare their names. Whomever's name comes last alphabetically is the winner.
 
 
-    highest_count = 0
-    winner = ""
+    '''
+    Old code from first pass solution:
 
     for name, count in vote_tally.items():
         if count == highest_count:
@@ -31,7 +39,7 @@ def count_votes(votes):
         elif count > highest_count:
             highest_count = count
             winner = name
-    
+    '''
     return winner
 
     # Return the name of the person with the highest number of votes
