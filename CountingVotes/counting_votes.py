ffba3003ce6b668450bc31b09317420bb9d611ed
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2019-06-06 15:13:34 -0400

    Instantiate name as name.lower() to avoid errors

--- CountingVotes/counting_votes.py
+++ CountingVotes/counting_votes.py
@@ -6,20 +6,21 @@ def count_votes(votes):
         return "No winner."
     elif len(votes) == 1:
         return votes[0]
+
     # Create dictionary to store key/value pairs of names and their vote count.
     vote_tally = {}
     highest_count = 0
     winner = ""
 
     # Loop over list, check to see if name (as a key) exists in the dictionary. If it doesn't, add it as a new key and add 1 as its starting value. If it does, increase the value by 1.
-
     # Edge case: If some names have uppercase and some lower case -- convert all entries to lowercase.
 
     for name in votes:
-        if name.lower() in vote_tally:
-            vote_tally[name.lower()] += 1
+        name = name.lower()
+        if name in vote_tally:
+            vote_tally[name] += 1
         else:
-            vote_tally[name.lower()] = 1
+            vote_tally[name] = 1
 
         if vote_tally[name] > highest_count:
             highest_count = vote_tally[name]
@@ -27,11 +28,13 @@ def count_votes(votes):
         elif vote_tally[name] == highest_count:
             winner = name if name > winner else winner
 
-    # Loop through the completed dictionary's values, and return the key with the highest number. If two names have an equal amount of votes, compare their names. Whomever's name comes last alphabetically is the winner.
+    # Return the name of the person with the highest number of votes
+    return winner
 
 
     '''
     Old code from first pass solution:
+    # Loop through the completed dictionary's values, and return the key with the highest number. If two names have an equal amount of votes, compare their names. Whomever's name comes last alphabetically is the winner.
 
     for name, count in vote_tally.items():
         if count == highest_count:
@@ -40,8 +43,6 @@ def count_votes(votes):
             highest_count = count
             winner = name
     '''
-    return winner
 
-    # Return the name of the person with the highest number of votes
 
-count_votes(['veronica', 'mary', 'alex', 'james', 'mary', 'michael', 'alex', 'michael'])
+print(count_votes(['veronica', 'mary', 'alex', 'james', 'mary', 'michael', 'alex', 'Michael']))
